774ca2e256b61ffc57d04c9439e030c3ad54e2df
kup21 <kup21@ic.ac.uk>
2022-03-20 09:27:36 +0000

    kup21 : Completed Battleships

--- Battleships/src/Grid.java
+++ Battleships/src/Grid.java
@@ -1,36 +1,96 @@
+import java.util.Arrays;
+
 public class Grid {
 
     private static final int WIDTH = 10;
     private static final int HEIGHT = 10;
 
     private final Piece[][] grid = new Piece[HEIGHT][WIDTH];
+    private int totalShipPieces = 0;
 
     public Grid() {
-        // TODO: Question 2a.
+        Arrays.stream(grid).forEach(row -> Arrays.fill(row, Piece.WATER));
+    }
+
+    public int getNumberofShips() {
+        return totalShipPieces;
     }
 
     public boolean canPlace(Coordinate c, int size, boolean isDown) {
-        return false; // TODO: delete this line for Question 2b.
+        boolean withinBounds;
+        boolean placeable = false;
+        if (isDown) {
+            withinBounds = c.getRow() + size <= HEIGHT;
+            for (int i = 0; i < size; i++) {
+                placeable = grid[c.getRow() + i][c.getColumn()] == Piece.WATER;
+            }
+        } else {
+            withinBounds = c.getColumn() + size <= WIDTH;
+            for (int i = 0; i < size; i++) {
+                placeable = grid[c.getRow()][c.getColumn() + i] == Piece.WATER;
+            }
+        }
+        return withinBounds && placeable;
     }
 
     public void placeShip(Coordinate c, int size, boolean isDown) {
-        // TODO: Question 2c.
+        if (canPlace(c, size, isDown)) {
+            if (isDown) {
+                for (int i = 0; i < size; i++) {
+                    grid[c.getRow() + i][c.getColumn()] = Piece.SHIP;
+                    totalShipPieces++;
+                }
+            } else {
+                for (int i = 0; i < size; i++) {
+                    grid[c.getRow()][c.getColumn() + i] = Piece.SHIP;
+                    totalShipPieces++;
+                }
+            }
+        }
     }
 
     public boolean wouldAttackSucceed(Coordinate c) {
-        return false; // TODO: delete this line for Question 3a.
+        return grid[c.getRow()][c.getColumn()] == Piece.SHIP;
     }
 
     public void attackCell(Coordinate c) {
-        // TODO: Question 3b.
+        Piece piece = grid[c.getRow()][c.getColumn()];
+        switch (piece) {
+            case SHIP -> {
+                piece = Piece.DAMAGED_SHIP;
+                totalShipPieces--;
+            }
+            case WATER -> piece = Piece.MISS;
+            default -> System.out.println("-->Already attacked this Coordinate.\n");
+        };
+        grid[c.getRow()][c.getColumn()] = piece;
+
+//        if (grid[c.getRow()][c.getColumn()] == Piece.SHIP) {
+//            grid[c.getRow()][c.getColumn()] = Piece.DAMAGED_SHIP;
+//            totalShipPieces--;
+//        }
+//        if (grid[c.getRow()][c.getColumn()] == Piece.WATER) {
+//            grid[c.getRow()][c.getColumn()] = Piece.MISS;
+//        }
+//
+
     }
 
     public boolean areAllSunk() {
-        return false; // TODO: delete this line for Question 3c.
+        return totalShipPieces == 0;
+//        boolean anyShip = true;
+//        for (Piece[] row : grid) {
+//            for (Piece p : row) {
+//                anyShip = p == Piece.SHIP;
+//            }
+//        }
+//        return !(anyShip);
     }
 
     public String toPlayerString() {
-        return null; // TODO: delete this line for Question 4.
+        Piece[][] playerGrid = Util.deepClone(grid);
+        Util.hideShips(playerGrid);
+        return renderGrid(playerGrid);
     }
 
     @Override
@@ -67,5 +127,12 @@ public class Grid {
         }
 
         return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Grid grid = new Grid();
+        System.out.println(grid);
+        grid.placeShip(Util.parseCoordinate("A7"), 2, false);
+        System.out.println(grid);
     }
 }
